27850f68d712362222924dd2bd09b85391ad8562
jesus re <eisa.eisa.r.r@gmail.com>
2021-08-04 21:09:24 +0430

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import GetStartedPage from "./pages/getStarted/GetStartedPage";
 import ItemInfo from "./pages/item/ItemInfo";
 import Products from "./pages/products/Products";
