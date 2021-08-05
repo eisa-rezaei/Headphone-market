d6f59f2470297549de04706587fb5484fab21d37
jesus re <eisa.eisa.r.r@gmail.com>
2021-08-05 17:54:09 +0430

    using context for userfavorite

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,6 +4,7 @@ import { data } from "../data/data";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useFavorites } from "../stogre/addToLikes";
 // import "../pages/products/Products.css";
 
 const Slider = () => {
@@ -17,27 +18,42 @@ const Slider = () => {
     slidesPerView: 1.5,
   };
 
+  const favoritesContext = useFavorites();
+
+  const toggleFavoriteStatusHandler = (id) => {
+    const isFavorite = favoritesContext.itemIsFavorite(id);
+    const userFavorite = products.filter((product) => product.id !== id);
+    if (isFavorite) {
+      favoritesContext.removeFavorite(id);
+    } else {
+      favoritesContext.addFavorite({ userFavorite });
+    }
+  };
+  console.log(favoritesContext.favorites);
   return (
     <section className="products-container">
       <Swiper {...params}>
-        {products.map((singleProduct, index) => {
+        {products.map((singleProduct) => {
           const { img, title, price, id } = singleProduct;
           return (
-            <Link to={`/item/${id}`} key={id}>
-              <div className="single-product">
+            <div className="single-product" key={id}>
+              <Link to={`/item/${id}`}>
                 <img src={img} alt={title} className="headphone-pic" />
-                <span className="like-icon">
-                  <AiOutlineHeart />
-                </span>
-                <div className="product-description">
-                  <h5>{title}</h5>
-                  <h4>{price}</h4>
-                  <span>
-                    <BsArrowRight />
-                  </span>
-                </div>
+              </Link>
+              <span
+                className="like-icon"
+                onClick={() => toggleFavoriteStatusHandler(id)}
+              >
+                <AiOutlineHeart />
+              </span>
+              <div className="product-description">
+                <h5>{title}</h5>
+                <h4>{price}</h4>
+                <Link to={`/item/${id}`}>
+                  <BsArrowRight />
+                </Link>
               </div>
-            </Link>
+            </div>
           );
         })}
       </Swiper>
